c925e1af0459b434cd8508c5e8d858a2a61529f8
Benjamin Wunderlich <bw@shopwerft.com>
2012-09-10 16:27:26 +0200

    Added: First simple LogMailer implementation

---
+++ app/code/local/Shopwerft/LogMailer/Helper/Data.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @category Shopwerft
+ * @package Shopwerft_LogMailer
+ * @authors Shopwerft GmbH <info@shopwerft.com>
+ * @developer Benjamin Wunderlich <b.wunderlich@shopwerft.com, http://www.shopwerft.com/>
+ * @version 0.1.0
+ * @copyright Shopwerft GmbH
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Shopwerft_LogMailer_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+    const CONFIG_PATH_ACTIVE         = 'dev/log/logmailingactive';
+    const CONFIG_PATH_LOGFILES       = 'dev/log/logfiles';
+    const CONFIG_PATH_RECEIVERS      = 'dev/log/receivers';
+
+    /**
+     * Check if sending out of log files is enabled.
+     *
+     * @param Mage_Core_Model_Store $store
+     * @return bool
+     */
+    public function isActive(Mage_Core_Model_Store $store = null)
+    {
+        return (bool) Mage::getStoreConfig(self::CONFIG_PATH_ACTIVE, $store);
+    }
+
+    /**
+     * Get the configured log files to observe.
+     *
+     * @param Mage_Core_Model_Store $store
+     * @return array The log files configured in backend
+     */
+    public function getLogFiles(Mage_Core_Model_Store $store = null)
+    {
+        $config = Mage::getStoreConfig(self::CONFIG_PATH_LOGFILES, $store);
+        return array_map('trim', explode("\n", $config));
+    }
+
+    /**
+     * Get the notification receivers (email-addresses).
+     *
+     * @param Mage_Core_Model_Store $store
+     * @return array
+     */
+    public function getReceivers(Mage_Core_Model_Store $store = null)
+    {
+        $config = Mage::getStoreConfig(self::CONFIG_PATH_RECEIVERS, $store);
+        return array_map('trim', explode("\n", $config));
+    }
+}
---
+++ app/code/local/Shopwerft/LogMailer/Model/Filter.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @category Shopwerft
+ * @package Shopwerft_LogMailer
+ * @authors Shopwerft GmbH <info@shopwerft.com>
+ * @developer Benjamin Wunderlich <b.wunderlich@shopwerft.com, http://www.shopwerft.com/>
+ * @version 0.1.0
+ * @copyright Shopwerft GmbH
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Shopwerft_LogMailer_Model_Filter extends Mage_Core_Model_Abstract
+{
+
+    protected $_files = array();
+
+    /**
+     * Add a single (string) or multiple log filenames (array) to filter.
+     * @param string|array $filename
+     * @return Shopwerft_LogMailer_Model_Filter
+     */
+    public function addLogfile($filename)
+    {
+        if(is_array($filename)) {
+            $this->_files = array_merge($this->_files, $filename);
+        } elseif(is_string($filename)) {
+            $this->_files[] = $filename;
+        }
+        return $this;
+    }
+
+    /**
+     * Get the log file lines as a report.
+     *
+     * @return string
+     */
+    public function getLines()
+    {
+        $resultLines = "Log Messages from ". $this->getDate();
+        $logDir = Mage::getBaseDir('var').DS.'log'.DS;
+        foreach($this->_files as $filename) {
+            $file = $logDir . $filename;
+            if(!file_exists($file)) {
+                $resultLines .= "\n\nFile $filename does not exist!";
+                continue;
+            }
+
+            $lines = file($file);
+
+            $resultLines .= "\n\nFile $filename:\n\n";
+
+            $matches = false;
+            $linesCount = 0;
+            foreach ($lines as $line_num => $line) {
+                if(preg_match('#\A\d\d\d\d-\d\d-\d\d#', $line)) {
+                    if(substr($line, 0, 10) == $this->getDate()) {
+                        $matches = true;
+                    } else {
+                        $matches = false;
+                    }
+                }
+                if($matches) {
+                    $linesCount++;
+                    $resultLines .= $line;
+                }
+            }
+
+            if($linesCount == 0) {
+                $resultLines .= "---";
+            }
+
+        }
+
+        return $resultLines;
+    }
+
+
+}
